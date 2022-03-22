d070105ba60ba88ba28d7f4c0fc2acc2bd0ceff6
Marta Martínez <martinezrochermarta@gmail.com>
2022-03-22 18:20:52 +0100

    Intro php / js / menu
    change the project to Mamp, add hamburguer menu with js and php

---
+++ php/nav.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Menu</title>
+</head>
+<body>
+    
+<span class="nav-bar" id="btnMenu"><img src="menu.svg" alt=""></span>
+    <nav class="main-nav">
+        <ul class="menu" id="menu">
+            <li class="menu__item"><a href="" class="menu__link">Home</a></li>
+            <li class="menu__item"><a href="" class="menu__link">Descubre la app</a></li>
+            <li class="menu__item"><a href="" class="menu__link">Ubicaciones</a></li>
+            <li class="menu__item"><a href="" class="menu__link">Área de usuario</a></li>
+        </ul>
+    </nav>
+    <script src="/menu.js"></script>
+</body>
+</html>
