9475de7bfcfa9e53fa00a7129f7464365db33929
Marta Martínez <martinezrochermarta@gmail.com>
2022-04-07 18:18:25 +0200

    last changes

--- js/user.js
+++ js/user.js
@@ -1,22 +1,28 @@
 const params = new URLSearchParams(window.location.search);
 const userId = params.get('name');
 
+
 axios.get(`../json/usuarios.json`)
     .then(function(response) {
         
-        const selectedUser = response.data.find(function(user){ return user.username === userId; })
-        document.querySelector('.name').textContent = selectedUser.name;
-        document.querySelector('.email').textContent = selectedUser.email;
-        document.querySelector('.username').textContent = selectedUser.username;
-        document.querySelector('.imguser').src = selectedUser.imguser;
-        document.querySelector('.sld1').src = selectedUser.sld1;
-        document.querySelector('.sld2').src = selectedUser.sld2;
-        document.querySelector('.sld3').src = selectedUser.sld3;
-        document.querySelector('.sld4').src = selectedUser.sld4;
-        document.querySelector('.sld5').src = selectedUser.sld5;
-        document.querySelector('.sld6').src = selectedUser.sld6;
-        document.querySelector('.sld7').src = selectedUser.sld7;
-        document.querySelector('.sld8').src = selectedUser.sld8;
-        document.querySelector('.sentence').textContent = selectedUser.sentence;
+        const selectedUser = response.data.find(function(user){ return user.username === userId; });
+        if (selectedUser == null){
+            window.location.href='/login'
+        }
+        else {
 
+            document.querySelector('.name').textContent = selectedUser.name;
+            document.querySelector('.email').textContent = selectedUser.email;
+            document.querySelector('.username').textContent = selectedUser.username;
+            document.querySelector('.imguser').src = selectedUser.imguser;
+            document.querySelector('.sld1').src = selectedUser.sld1;
+            document.querySelector('.sld2').src = selectedUser.sld2;
+            document.querySelector('.sld3').src = selectedUser.sld3;
+            document.querySelector('.sld4').src = selectedUser.sld4;
+            document.querySelector('.sld5').src = selectedUser.sld5;
+            document.querySelector('.sld6').src = selectedUser.sld6;
+            document.querySelector('.sld7').src = selectedUser.sld7;
+            document.querySelector('.sld8').src = selectedUser.sld8;
+            document.querySelector('.sentence').textContent = selectedUser.sentence;
+        }
     });
--- php/footer.php
+++ php/footer.php
@@ -1,6 +1,6 @@
 <footer class="main-footer">
 <div class="main-footer main-footer__icons">
-    <p class="small-text">2022 @ | Powered by <a href="https://frontend.esatid3-2021.com/marta/" class="link-footer">Marta Martíbez Rocher</a></li> | all rights reserved</p>
+    <p class="small-text">2022 @ | Powered by <a href="https://frontend.esatid3-2021.com/marta/" class="link-footer">Marta Martínez Rocher</a></li> | all rights reserved</p>
     <div>
     <img src="<?php echo $path; ?>resources/twitter-icon.png" alt="twitter-icon" width="40px">
     <img src="<?php echo $path; ?>resources/instagram-icon.png" alt="instagram-icon" width="40px">
